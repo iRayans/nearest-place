ce19964283cac5a7d79f18d85e5424952ea395d4
rharbi <iirharbi@gmail.com>
2025-07-26 23:31:31 +0300

    refactor/ move Center , Circle to clasess

---
+++ src/main/java/com/rayan/nearestrest/dto/Center.java
@@ -0,0 +1,31 @@
+package com.rayan.nearestrest.dto;
+
+public class Center {
+    private Double latitude;
+    private Double longitude;
+
+
+    public Center() {
+    }
+
+    public Center(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ src/main/java/com/rayan/nearestrest/enumeration/PriceLevel.java
@@ -0,0 +1,8 @@
+package com.rayan.nearestrest.enumeration;
+
+public enum PriceLevel {
+    PRICE_LEVEL_INEXPENSIVE,
+    PRICE_LEVEL_MODERATE,
+    PRICE_LEVEL_EXPENSIVE,
+    PRICE_LEVEL_VERY_EXPENSIVE
+}
